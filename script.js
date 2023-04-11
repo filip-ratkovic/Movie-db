1f1c2686ff385ae0726a70c34dab80ee88410f73
Filip <filip.ratkovic.fr@gmail.com>
2023-04-11 13:49:01 +0200

    Home page main photo

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const genresURL = 'https://api.themoviedb.org/3/genre/movie/list?api_key=2c2ddf06e3672c277286fe290e3b4cec&language=en-US'
 const popularURL = 'https://api.themoviedb.org/3/movie/popular?api_key=2c2ddf06e3672c277286fe290e3b4cec&language=en-US&page=1'
+const imgURL = "https://image.tmdb.org/t/p/w1280";
+const searchURL ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const detailsURL ='https://api.themoviedb.org/3/movie/'
+const api_key = '?api_key=2c2ddf06e3672c277286fe290e3b4cec&language=en-US'
 
 const genresEl = document.getElementById('genres-list');
 const homePage = document.getElementById('home-main');
+const showedMainCont = document.getElementById('showed-main-cont');
 
 async function getGenresListData() {
     const genresData = await fetch(genresURL);
@@ -19,25 +24,79 @@ function getGenresList(data) {
         genresEl.appendChild(genreLi)
     })
 }
+const movieID = 'https://api.themoviedb.org/3/movie/502356?api_key=2c2ddf06e3672c277286fe290e3b4cec&language=en-US'
 
 async function getPopularMovieData() {
     const popularData = await fetch(popularURL);
     const popularDataRes = await popularData.json();
-    getPopularMovie(popularDataRes);
+    // getPopularMovie(popularDataRes);
+        getPopularMovie(popularDataRes);
 }
 
 function getPopularMovie(data) {
+   const mainMovie = data.results[0].id
     // data.results.forEach((movie) => {
-    //     const title = document.createElement('h1')
-    //     const img = document.createElement('img')
-    //     const text = document.createElement('p')
+        
+    //     const card = document.createElement('div');
+    //     card.innerHTML = `
+    //     <h1>${movie.title}</h1>
+    //     <p>${movie.overview}</p>
+    //     <img src="${imgURL + movie.poster_path}" alt="">
+    //     `
 
-    //     title.innerHTML = movie.title;
-    //     img.src = `https://api.themoviedb.org/t/p${movie.poster_path}`
-    //     text.innerHTML = movie.overview;
-
-    //     homePage.appendChild(title, img, text);
+    //     homePage.appendChild(card)
     // })
+    getMainMovieData(mainMovie)
+}
+
+
+
+async function getMainMovieData(movieID) {
+    const mainMovieData = await fetch(`${detailsURL}${movieID}${api_key}`);
+    const mainMovieDataRes = await mainMovieData.json();
+        homePageMovie(mainMovieDataRes);
+}
+
+function homePageMovie(data) {
+        showedMainCont.style.backgroundImage  = `linear-gradient(to left, rgba(245, 246, 252, 0.0) 0%, rgba(0, 0, 0, 0.9)70% , rgba(0, 0, 0) 100%),
+        url('https://image.tmdb.org/t/p/w1280/${data.backdrop_path}')`
+        
+   
+        showedMainCont.innerHTML = `
+        <div class="showed-text-cont">
+        <h1>${data.title}</h1>
+        <div class="showed-rateDuration" id="showed-rateDuration">
+            <div class="showed-rate">
+              <i class="fa-solid fa-star"></i>
+            <p>${data.vote_average}</p>
+            </div>
+            <p>${data.release_date.slice(0,4)}</p>
+            <p>${data.runtime}</p>
+        </div>
+
+        <div class="showed-genres" id='showed-genres'>
+       
+        </div>
+
+        <div class="showed-overview">
+        ${data.overview}
+        </div>
+
+        <div class="showed-buttons">
+            <button id="watch-btn"> Watch <i class="fa-solid fa-play"></i></button>
+            <button id="add-btn"> Add  <i class="fa-solid fa-heart"></i></button>
+        </div>
+    </div>
+        `
+
+        {data.genres.map((genre)=> {
+            const showedGenres = document.getElementById('showed-genres');
+            const genresLi = document.createElement('li');
+            genresLi.innerHTML = genre.name;
+            showedGenres.appendChild(genresLi);
+         })}
+
+
 }
 
 getPopularMovieData()
