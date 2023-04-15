52eeca45edc322b73c39a2fc40c51d8c1ed40c82
Filip <filip.ratkovic.fr@gmail.com>
2023-04-16 00:05:25 +0200

    Nav bar

--- movie-db/src/components/Nav/Nav.jsx
+++ movie-db/src/components/Nav/Nav.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
-import './nav.css'
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import "./nav.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
+  const [activeSearch, setActiveSearch] = useState(false);
+
+  const handleSearchToggle = () => {
+      if (activeSearch) {
+        setActiveSearch(false);
+      } else {
+        setActiveSearch(true);
+      }
+  }
   return (
-    <main className="nav" id='nav'>
-      <Link to='/'>Home</Link>
-      <Link to='/movies'>Movie</Link>
-      <Link to='/series'>Series</Link>
-      
-    </main>
-  )
+    <main className="nav" id="nav">
+      <div className="nav-logo">
+        <img src="https://img.icons8.com/color/48/null/netflix.png" />
+      </div>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movie</Link>
+        <Link to="/series">Series</Link>
+      </div>
+        <div className="search-cont">
+          <form className={`nav-search ${activeSearch ? 'active' : ''}`}>
+          <input type="text" placeholder="Search" className={`nav-input ${activeSearch ? 'active' : ''}`}/>
+          <img src="https://img.icons8.com/ios/50/null/search--v1.png" id='nav-search-img' onClick= {handleSearchToggle} />
+        </form>
+        </div>
+     </main>
+  );
 }
 
-export default Nav
+export default Nav;
