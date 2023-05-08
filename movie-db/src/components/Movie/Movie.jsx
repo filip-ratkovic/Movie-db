6d78b221f7dd47cea65a7f7f26f8053f4cb6b212
Filip <filip.ratkovic.fr@gmail.com>
2023-05-08 19:30:17 +0200

    Movie

--- movie-db/src/components/Movie/Movie.jsx
+++ movie-db/src/components/Movie/Movie.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactPaginate from 'react-paginate'
 
+const movieURL = 
+'https://api.themoviedb.org/3/movie/popular?api_key=2c2ddf06e3672c277286fe290e3b4cec&language=en-US&page=1'
+
 function Movie() {
+  const [movieData, setMovieData] = useState([]);
+  const [apiData, setAPIData] = useState([]);
+  
+
+  const fetchMovieData = async () => {
+    try {
+      const data = await fetch(movieURL);
+      const resData = await data.json();
+      setMovieData(resData.results);
+      setAPIData(resData)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  useEffect(() => {
+    
+   fetchMovieData()
+  }, [])
+  
+console.log(movieData)
+
   const handlePageChange = (data) => {
     console.log(data.selected)
   }
   return (
    <main>
-    
+      {movieData.map((movie) => {
+        return (
+          <div className="movie-card">
+            <h1>{movie.title}</h1>
+          </div>
+        )
+      })}
 
       <ReactPaginate
         previousLabel={"<<"}
@@ -17,6 +49,14 @@ function Movie() {
         marginPagesDisplayed={1}
         pageRangeDisplayed={3}
         onPageChange={handlePageChange}
+        containerClassName='pagination justify-content-center'
+        pageClassName='page-item'
+        pageLinkClassName='page-link'
+        previousClassName='page-item'
+        previousLinkClassName='page-link'
+        nextClassName='page-item'
+        nextLinkClassName='page-link'
+        activeClassName='active'
       />
    </main>
   )
